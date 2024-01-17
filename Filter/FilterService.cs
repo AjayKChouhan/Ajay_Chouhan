9ed7ae58348ddea8f166817ddf90994aa53847cb
AjayKChouhan <156907572+AjayKChouhan@users.noreply.github.com>
2024-01-17 17:45:10 +0530

    Upload FilterService.cs

--- Filter/FilterService.cs
+++ Filter/FilterService.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
-namespace ajay_chouhan.Filter
+namespace Ajay_Chouhan.Filter
 {
     public static class FilterService<TEntity>
     {
