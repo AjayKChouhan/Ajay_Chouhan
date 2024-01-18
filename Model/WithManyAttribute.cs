dbc88eff76cda17396228e7d5e41e6bf023d2f29
AjayKChouhan <156907572+AjayKChouhan@users.noreply.github.com>
2024-01-18 10:52:30 +0530

    Upload WithManyAttribute.cs

---
+++ Model/WithManyAttribute.cs
@@ -0,0 +1,11 @@
+namespace Aja_Chouhan.Model
+{
+    internal class WithManyAttribute : Attribute
+    {
+        private string v;
+        public WithManyAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
