12a496e4fbcca9e81b94da1fc8c65aac71671582
AjayKChouhan <156907572+AjayKChouhan@users.noreply.github.com>
2024-02-02 12:07:42 +0530

    Upload FilterCriteria.cs

---
+++ Filter/FilterCriteria.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ajay_Chouhan.Filter
+{
+    public class FilterCriteria
+    {
+        public string PropertyName { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+    }
+}
